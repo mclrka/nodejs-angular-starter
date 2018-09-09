b0bd81b1482ed687ede7f5197d78675c252e9ca8
Shynet <shynet@gmail.com>
2018-09-09 18:43:40 +0300

    Working on angular universal - still not working

--- src/ng-app.ts
+++ src/ng-app.ts
@@ -1,18 +1,19 @@
-import { AppServerModule } from "../angular-src/src/app/app.server.module";
 // These are important and needed before anything else
-import "zone.js/dist/zone-node";
-import "reflect-metadata";
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
 
-import { renderModuleFactory } from "@angular/platform-server";
-import { enableProdMode } from "@angular/core";
+import { enableProdMode } from '@angular/core';
 
-import * as express from "express";
-import { join } from "path";
-import { readFileSync } from "fs";
+import * as express from 'express';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+
+// Express Engine
+import { ngExpressEngine } from '@nguniversal/express-engine';
 
 // Import module map for lazy loading
-import { provideModuleMap } from "@nguniversal/module-map-ngfactory-loader";
-import { Application } from "express";
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { Application } from 'express';
 
 /**
  * Use Angular-Universal to deliever our app.
@@ -21,39 +22,35 @@ export class AngularApp {
   init(app: Application) {
     enableProdMode();
 
-    const DIST_FOLDER = join(__dirname, "dist");
+    const DIST_FOLDER = join(__dirname, 'dist');
 
     const {
       AppServerModuleNgFactory,
       LAZY_MODULE_MAP
-    } = require("./dist/server/server");
+    } = require('./dist/server/main');
 
     // Our index.html we'll use as our template
     const template = readFileSync(
-      join(DIST_FOLDER, "browser", "index.html")
+      join(DIST_FOLDER, 'browser', 'index.html')
     ).toString();
 
-    app.engine("html", (_, options, callback) => {
-      renderModuleFactory(AppServerModuleNgFactory, {
-        // Our index.html
-        document: template,
-        url: options.req.url,
-        // DI so that we can get lazy-loading to work differently (since we need it to just instantly render it)
-        extraProviders: [provideModuleMap(LAZY_MODULE_MAP)]
-      }).then(html => {
-        callback(null, html);
-      });
-    });
+    app.engine(
+      'html',
+      ngExpressEngine({
+        bootstrap: AppServerModuleNgFactory,
+        providers: [provideModuleMap(LAZY_MODULE_MAP)]
+      })
+    );
 
-    app.set("view engine", "html");
-    app.set("views", join(DIST_FOLDER, "browser"));
+    app.set('view engine', 'html');
+    app.set('views', join(DIST_FOLDER, 'browser'));
 
     // Server static files from /browser
-    app.get("*.*", express.static(join(DIST_FOLDER, "browser")));
+    app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
 
     // All regular routes use the Universal engine
-    app.get("*", (req, res) => {
-      res.render(join(DIST_FOLDER, "browser", "index.html"), { req });
+    app.get('*', (req, res) => {
+      res.render('index', { req });
     });
   }
 }
